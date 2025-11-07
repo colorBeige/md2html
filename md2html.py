a18761860ba5d5fe289406bdda94f5e6db657f9f
Cody <133912252+colorBeige@users.noreply.github.com>
2025-11-06 19:58:52 -0500

    Add function placeholders

---
+++ md2html.py
@@ -0,0 +1,29 @@
+def convert_emphasis(text: str) -> str:
+    pass
+
+def convert_paragraph(text: str) -> str:
+    pass
+
+def convert_headings(text: str) -> str:
+    pass
+
+def convert_ordered_list(text: str) -> str:
+    pass
+
+def convert_unordered_list(text: str) -> str:
+    pass
+
+def convert_code(text: str) -> str:
+    pass
+
+def convert_link(text: str) -> str:
+    pass
+
+def convert(text: str) -> str:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
